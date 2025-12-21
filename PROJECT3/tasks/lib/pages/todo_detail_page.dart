fa497b455208d8075c83f19cf35be995dc52e374
JIMIN0001 <theone20181@gmail.com>
2025-12-21 17:16:37 +0900

    feat: update todo detail and UI
    - Convert todo detail to StatefulWidget

--- PROJECT3/tasks/lib/pages/todo_detail_page.dart
+++ PROJECT3/tasks/lib/pages/todo_detail_page.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import '../models/todo_entity.dart';
 
 // 할 일 하나의 상세 내용을 보여주는 페이지
-class ToDoDetailPage extends StatelessWidget {
+class ToDoDetailPage extends StatefulWidget {
   // 이전 화면(HomePage)에서 전달받은 할 일 데이터
   final ToDoEntity todo;
-
   const ToDoDetailPage({super.key, required this.todo});
+
+  @override
+  State<ToDoDetailPage> createState() => _ToDoDetailPageState();
+}
+
+class _ToDoDetailPageState extends State<ToDoDetailPage> {
+  // 현재 페이지의 상태를 관리할 변수 (현재 페이지 반영을 위해 필요)
+  late ToDoEntity _currentTodo = widget.todo;
 
   @override
   Widget build(BuildContext context) {
@@ -19,30 +26,31 @@ class ToDoDetailPage extends StatelessWidget {
         leading: BackButton(
           color: const Color(0xFF8D7B68),
           onPressed: () {
-            Navigator.pop(context, todo);
+            // 뒤로 갈 때 최종적으로 변경된 데이터를 홈으로 전달 (과제 요건 준수)
+            Navigator.pop(context, _currentTodo);
           },
         ),
         actions: [
           // 즐겨찾기 상태 토글 버튼
           IconButton(
             icon: Icon(
-              todo.isFavorite ? Icons.star : Icons.star_border,
+              _currentTodo.isFavorite ? Icons.star : Icons.star_border,
               color: const Color(0xFF8D7B68),
             ),
             onPressed: () {
-              // 즐겨찾기 값을 변경한 todo를 만들어 이전 화면으로 전달
-              Navigator.pop(
-                context,
-                todo.copyWith(isFavorite: !todo.isFavorite),
-              );
+              // 현재 페이지에 즉시 반영하기 위해 setState 사용
+              setState(() {
+                _currentTodo = _currentTodo.copyWith(
+                  isFavorite: !_currentTodo.isFavorite,
+                );
+              });
             },
           ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        // 할 일의 상세 설명 표시 (없으면 빈 문자열)
-        child: Text(todo.description ?? ''),
+        child: Text(_currentTodo.description ?? ''), // 변경된 상태의 데이터를 표시
       ),
     );
   }
